370a8a69d6c17b21bdac2074fa4da6b3db6c6c0b
Kaivalya Deshpande <63287674+KaivalyaD@users.noreply.github.com>
2025-05-17 14:05:26 +0530

    Delete machine_language/01_nop/romgen.py

--- machine_language/01_nop/romgen.py
+++
@@ -1,8 +0,0 @@
-EEPROM_SIZE = 2**15;  # AT28C256 is a 32K EEPROM
-
-rom = bytearray(
-    [0xEA] * EEPROM_SIZE  # 0xea is the opcode for a 6502 NOP instruction
-)
-
-with open("./rom.bin", "wb") as binfile:
-    binfile.write(rom)
